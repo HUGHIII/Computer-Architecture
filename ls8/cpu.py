1803f2869fd3062516fca4595f7b875d16c6ab72
Trey Lloyd <58454082+HUGHIII@users.noreply.github.com>
2020-09-15 15:41:50 -0500

    adding cpu class and ramwrite-ramread methods

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.register = [0] * 8
+
 
     def load(self):
         """Load a program into memory."""
@@ -30,6 +32,12 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
+
+    def ram_read(self,MAR):
+        return self.ram[MAR]
+
+    def ram_write(self,MAR,MDR)
+        self.ram[MAR] = MDR
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
