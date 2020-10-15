15e517d4d2410ad663d23890c4576351d9bb777c
Trey Lloyd <58454082+HUGHIII@users.noreply.github.com>
2020-10-15 15:27:12 -0500

    fix mult in alu

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,16 +1,13 @@
+  
 """CPU functionality."""
 
 import sys
+# fileN = sys.argv[1]
+# print(fileN,'sys argv')
 
-HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
-MUL = 0b10100010
-POP = 0b01000110
-PUSH  = 0b01000101
-ADD = 0b10100000
-CALL = 0b01010000 
-RET = 0b00010001
+HLT = 0b00000001
 
 class CPU:
     """Main CPU class."""
@@ -18,14 +15,10 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
-        self.register = [0] * 8
+        self.reg = [0] * 8
         self.pc = 0
         self.running = True
-        self.branchtable = {
-            HLT : self.handleHLT,
-            LDI : self.handleLDI,
-            PRN : self.handlePRN
-        }
+
 
 
 
@@ -33,19 +26,41 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
-        fl = sys.argv[1]
 
-        if fl:
-            with open(fl) as f:
-                for ln in f:
-                    ln = ln.split('#')
-                    if ln[0] == '':
+        if len(sys.argv) != 2:
+            print("Usage: ls8.py examples/file")
+            sys.exit(1)
+
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    line = line.strip()
+                    # print(line)
+                    if line == "" or line[0] == "#":
                         continue
-                    self.ram[address] = int(ln[0],2)
+
+                    try:
+                        str_value = line.split("#")[0]
+                        value = int(str_value, 2)
+                        
+
+                    except ValueError:
+                        print(f"Invalid Number: {str_value}")
+                        sys.exit(1)
+
+                    self.ram[address] = value
                     address += 1
 
-        else:
-            print('no argv') 
+        except FileNotFoundError:
+            print(f"FileNotFound: {sys.argv[1]}")
+            sys.exit(2)
+
+        for i in self.ram:
+
+            print(i)
+
+
+
 
         # For now, we've just hardcoded a program:
 
@@ -64,23 +79,14 @@ class CPU:
         #     address += 1
 
 
-    def ram_read(self,MAR):
-        return self.ram[MAR]
-
-    def ram_write(self,MAR,MDR):
-        self.ram[MAR] = MDR
-
-    def handleHLT(self):
-        self.running = False
-
-    def handleLDI(self,)
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -104,17 +110,37 @@ class CPU:
 
         print()
 
+
+    def ram_read(self,MAR):
+        return self.ram[MAR]
+
+    def ram_write(self,MDR,MAR):
+        self.ram[MAR] = MDR
+
     def run(self):
         """Run the CPU."""
-        # IR local variable in run method
-        # make IR = mem address in pc reg
-        
-        IR = self.ram_read(self.pc)
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-        if IR not in self.branchtable:
-            print('no IR at mar')
-        else:
-            run = self.branchtable[IR]
-            run(operand_a, operand_b)
-        
+        while self.running:
+            # instruction register, read memory address stored in register
+            # for i in range(256):
+            #     print(self.ram[i],end = "--")
+
+            # self.trace()
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR == HLT:
+                # exit
+                self.running = False
+            elif IR == LDI:
+                # set specified register to specified value
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+                
+            elif IR == PRN:
+                # print value from specified register
+                print(self.reg[operand_a])
+                self.pc += 2
+            else:
+                print(f'unknown instruction {IR} at address {self.pc}')
+                self.running = False 
